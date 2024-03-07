72c28856165ac4f413ec1646723ad64964484a23
Schmeink, Jens <jens.schmeink@dlr.de>
2024-03-07 12:55:40 +0000

    Resolve "DataType for byteArrays"

---
+++ src/intelli/data/bytearraydata.h
@@ -0,0 +1,34 @@
+/* GTlab - Gas Turbine laboratory
+ * Source File:
+ * copyright 2009-2023 by DLR
+ *
+ *  Created on: 07.03.2024
+ *  Author: Jens Schmeink (AT-TWK)
+ *  Tel.: +49 2203 601 2191
+ */
+#ifndef BYTEARRAYDATA_H
+#define BYTEARRAYDATA_H
+
+#include "intelli/nodedata.h"
+
+#include <QByteArray>
+
+namespace intelli
+{
+
+class ByteArrayData : public NodeData
+{
+    Q_OBJECT
+public:
+    Q_INVOKABLE explicit ByteArrayData(const QByteArray& ds = {}) :
+        NodeData("python var"),
+        m_data(ds) {}
+
+    QByteArray data() const { return m_data; }
+
+private:
+    QByteArray m_data;
+};
+
+} // namespace intelli
+#endif // BYTEARRAYDATA_H
