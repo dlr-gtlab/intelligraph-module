93141e0240afe784f039a11bb98029ddd4e61c6d
Schmeink, Jens <Jens.Schmeink@dlr.de>
2025-03-18 09:29:52 +0100

    first version based on append to defined parent target

--- src/intelli/node/objectsink.h
+++ src/intelli/node/objectsink.h
@@ -10,7 +10,10 @@
 #ifndef GT_INTELLI_OBJECTSINK_H
 #define GT_INTELLI_OBJECTSINK_H
 
+#include "gt_objectlinkproperty.h"
 #include "intelli/node.h"
+
+class QPushButton;
 
 namespace intelli
 {
@@ -27,6 +30,18 @@ protected:
 
 private:
     PortId m_in;
+
+    GtObjectLinkProperty m_target;
+
+    void setButtonColor(QPushButton* button, const QColor& col);
+
+private slots:
+    void doExport();
+
+signals:
+    void exportActivated(bool);
+
+    void exportFinished(bool);
 };
 
 } // namespace intelli
