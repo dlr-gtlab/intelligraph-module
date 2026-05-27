dddf12cfbbaa913a8ce9bc5883ad7f175126d95b
Martin Siggel <martin.siggel@dlr.de>
2026-05-27 14:50:42 +0200

    Extract generic calculator exec widget

--- src/intelli/gui/ui/node/genericcalculatorexecnodeui.cpp
+++ src/intelli/gui/ui/node/genericcalculatorexecnodeui.cpp
@@ -20,7 +20,84 @@
 #include <QGraphicsWidget>
 #include <QVBoxLayout>
 
+#include <cassert>
+
 using namespace intelli;
+
+GenericCalculatorExecNodeWidget::GenericCalculatorExecNodeWidget(GenericCalculatorExecNode& node,
+                                                                 QWidget* parent) :
+    QWidget(parent)
+{
+    m_node = &node;
+
+    auto* lay = new QVBoxLayout(this);
+    lay->setContentsMargins(0, 0, 0, 0);
+
+    m_classEdit = new QComboBox(this);
+    m_classEdit->addItems(GenericCalculatorExecNode::classIdents());
+    m_classEdit->setStyleSheet(gt::gui::stylesheet::comboBox());
+    lay->addWidget(m_classEdit);
+
+    auto* model = exec::nodeDataInterface(*m_node);
+    GtObject* scope = model ? model->scope() : static_cast<GtObject*>(gtApp->currentProject());
+    assert(scope);
+
+    m_view = new GtPropertyTreeView(scope, this);
+    m_view->setAnimated(false);
+    lay->addWidget(m_view);
+
+    QObject::connect(m_classEdit, &QComboBox::currentTextChanged,
+                     this, &GenericCalculatorExecNodeWidget::updateClassFromEditor);
+    QObject::connect(m_node, &GenericCalculatorExecNode::classNameChanged,
+                     this, &GenericCalculatorExecNodeWidget::updateClassEditorFromNode);
+    QObject::connect(m_node, &GenericCalculatorExecNode::currentObjectChanged,
+                     this, &GenericCalculatorExecNodeWidget::updateCurrentObjectView);
+
+    m_node->syncConnectedPorts();
+
+    m_node->className().isEmpty() ? updateClassFromEditor(m_classEdit->currentText())
+                                  : updateClassEditorFromNode(m_node->className());
+    updateCurrentObjectView();
+}
+
+NodeUI::QGraphicsWidgetPtr
+GenericCalculatorExecNodeWidget::create(Node& source, NodeGraphicsObject& object)
+{
+    auto* node = qobject_cast<GenericCalculatorExecNode*>(&source);
+    if (!node) return nullptr;
+
+    auto w = std::make_unique<GenericCalculatorExecNodeWidget>(*node);
+    return NodeUI::convertToGraphicsWidget(std::move(w), object);
+}
+
+void
+GenericCalculatorExecNodeWidget::updateCurrentObjectView()
+{
+    m_view->setObject(nullptr);
+
+    auto obj = m_node->currentObject();
+    if (!obj) return;
+
+    m_view->setObject(obj);
+    // collapse first category
+    m_view->collapse(m_view->model()->index(0, 0, m_view->rootIndex()));
+
+    QObject::connect(obj, qOverload<GtObject*, GtAbstractProperty*>(&GtObject::dataChanged),
+                     m_node, &GenericCalculatorExecNode::onCurrentObjectDataChanged,
+                     Qt::UniqueConnection);
+}
+
+void
+GenericCalculatorExecNodeWidget::updateClassFromEditor(QString const& classText)
+{
+    m_node->setClassName(GenericCalculatorExecNode::classNameFromIdent(classText));
+}
+
+void
+GenericCalculatorExecNodeWidget::updateClassEditorFromNode(QString const& className)
+{
+    m_classEdit->setCurrentText(GenericCalculatorExecNode::identFromClassName(className));
+}
 
 GenericCalculatorExecNodeUI::GenericCalculatorExecNodeUI() = default;
 
@@ -29,63 +106,5 @@ GenericCalculatorExecNodeUI::centralWidgetFactory(Node const& n) const
 {
     if (!qobject_cast<GenericCalculatorExecNode const*>(&n)) return {};
 
-    return [this](Node& source, NodeGraphicsObject& object) -> QGraphicsWidgetPtr {
-        auto* node = qobject_cast<GenericCalculatorExecNode*>(&source);
-        if (!node) return nullptr;
-
-        auto w = std::make_unique<QWidget>();
-        auto* lay = new QVBoxLayout;
-        w->setLayout(lay);
-        lay->setContentsMargins(0, 0, 0, 0);
-
-        auto* edit = new QComboBox;
-        edit->addItems(GenericCalculatorExecNode::classIdents());
-        edit->setStyleSheet(gt::gui::stylesheet::comboBox());
-        lay->addWidget(edit);
-
-        auto* model = exec::nodeDataInterface(*node);
-        GtObject* scope = model ? model->scope() : static_cast<GtObject*>(gtApp->currentProject());
-        assert(scope);
-
-        auto* view = new GtPropertyTreeView(scope);
-        view->setAnimated(false);
-        lay->addWidget(view);
-
-        auto updateView = [this, view, node](){
-            view->setObject(nullptr);
-
-            auto obj = node->currentObject();
-            if (obj)
-            {
-                view->setObject(obj);
-                // collapse first category
-                view->collapse(view->model()->index(0, 0, view->rootIndex()));
-
-                QObject::connect(obj, qOverload<GtObject*, GtAbstractProperty*>(&GtObject::dataChanged),
-                                 node, &GenericCalculatorExecNode::onCurrentObjectDataChanged,
-                                 Qt::UniqueConnection);
-            }
-        };
-
-        auto const updateClass = [node, edit](){
-            node->setClassName(GenericCalculatorExecNode::classNameFromIdent(edit->currentText()));
-        };
-        auto const updateClassText = [node, edit](){
-            edit->setCurrentText(GenericCalculatorExecNode::identFromClassName(node->className()));
-        };
-
-        QObject::connect(edit, &QComboBox::currentTextChanged,
-                         edit, updateClass);
-        QObject::connect(node, &GenericCalculatorExecNode::classNameChanged,
-                         edit, updateClassText);
-        QObject::connect(node, &GenericCalculatorExecNode::currentObjectChanged,
-                         view, updateView);
-
-        node->syncConnectedPorts();
-
-        node->className().isEmpty() ? updateClass() : updateClassText();
-        updateView();
-
-        return convertToGraphicsWidget(std::move(w), object);
-    };
+    return &GenericCalculatorExecNodeWidget::create;
 }
--- src/intelli/gui/ui/node/genericcalculatorexecnodeui.h
+++ src/intelli/gui/ui/node/genericcalculatorexecnodeui.h
@@ -9,9 +9,39 @@
 #define GT_INTELLI_GENERICCALCULATOREXECNODEUI_H
 
 #include <intelli/gui/nodeui.h>
+#include <QWidget>
+
+class GtPropertyTreeView;
+class QComboBox;
 
 namespace intelli
 {
+
+class GenericCalculatorExecNode;
+class NodeGraphicsObject;
+
+class GenericCalculatorExecNodeWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit GenericCalculatorExecNodeWidget(GenericCalculatorExecNode& node, QWidget* parent = nullptr);
+
+    static NodeUI::QGraphicsWidgetPtr create(Node& source, NodeGraphicsObject& object);
+
+private slots:
+
+    void updateCurrentObjectView();
+    void updateClassFromEditor(QString const& classText);
+    void updateClassEditorFromNode(QString const& className);
+
+private:
+
+    GenericCalculatorExecNode* m_node{};
+    QComboBox* m_classEdit{};
+    GtPropertyTreeView* m_view{};
+};
 
 class GenericCalculatorExecNodeUI : public NodeUI
 {
