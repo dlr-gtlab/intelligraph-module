e28a1418c01e2430d6a4dee10528624fd2986c77
Martin Siggel <martin.siggel@dlr.de>
2026-05-27 14:54:35 +0200

    Extract number math widget

--- src/intelli/gui/ui/node/numbermathnodeui.cpp
+++ src/intelli/gui/ui/node/numbermathnodeui.cpp
@@ -8,14 +8,86 @@
 #include <intelli/gui/ui/node/numbermathnodeui.h>
 
 #include <intelli/gui/graphics/nodeobject.h>
-#include <intelli/gui/utilities.h>
 #include <intelli/node/numbermath.h>
 
 #include <QComboBox>
 #include <QGraphicsWidget>
-#include <QLayout>
+#include <QVBoxLayout>
 
 using namespace intelli;
+
+NumberMathNodeWidget::NumberMathNodeWidget(NumberMathNode& node, QWidget* parent) :
+    QWidget(parent)
+{
+    m_node = &node;
+
+    auto* lay = new QVBoxLayout(this);
+    lay->setContentsMargins(0, 0, 0, 0);
+
+    m_combo = new QComboBox(this);
+    lay->addWidget(m_combo);
+    m_combo->addItems(QStringList{"+", "-", "*", "/", "pow"});
+
+    QObject::connect(m_node, &NumberMathNode::operationChanged,
+                     this, &NumberMathNodeWidget::updateComboFromNode);
+    QObject::connect(m_combo, &QComboBox::currentTextChanged,
+                     this, &NumberMathNodeWidget::updateNodeFromCombo);
+
+    updateComboFromNode();
+}
+
+NodeUI::QGraphicsWidgetPtr
+NumberMathNodeWidget::create(Node& source, NodeGraphicsObject& object)
+{
+    auto* node = qobject_cast<NumberMathNode*>(&source);
+    if (!node) return nullptr;
+
+    auto w = std::make_unique<NumberMathNodeWidget>(*node);
+    return NodeUI::convertToGraphicsWidget(std::move(w), object);
+}
+
+QString
+NumberMathNodeWidget::toText(NumberMathNode::MathOperation op)
+{
+    switch (op)
+    {
+    case NumberMathNode::Minus:
+        return QStringLiteral("-");
+    case NumberMathNode::Divide:
+        return QStringLiteral("/");
+    case NumberMathNode::Multiply:
+        return QStringLiteral("*");
+    case NumberMathNode::Power:
+        return QStringLiteral("pow");
+    case NumberMathNode::Plus:
+        break;
+    }
+    return QStringLiteral("+");
+}
+
+NumberMathNode::MathOperation
+NumberMathNodeWidget::fromText(QString const& text)
+{
+    if (text == QStringLiteral("-")) return NumberMathNode::Minus;
+    if (text == QStringLiteral("*")) return NumberMathNode::Multiply;
+    if (text == QStringLiteral("/")) return NumberMathNode::Divide;
+    if (text == QStringLiteral("pow")) return NumberMathNode::Power;
+    return NumberMathNode::Plus;
+}
+
+void
+NumberMathNodeWidget::updateComboFromNode()
+{
+    m_combo->setCurrentText(toText(m_node->operation()));
+}
+
+void
+NumberMathNodeWidget::updateNodeFromCombo(QString const& text)
+{
+    auto next = fromText(text);
+    if (next == m_node->operation()) return;
+    m_node->setOperation(next);
+}
 
 NumberMathNodeUI::NumberMathNodeUI() = default;
 
@@ -24,56 +96,5 @@ NumberMathNodeUI::centralWidgetFactory(Node const& n) const
 {
     if (!qobject_cast<NumberMathNode const*>(&n)) return {};
 
-    return [this](Node& source, NodeGraphicsObject& object) -> QGraphicsWidgetPtr {
-        auto* node = qobject_cast<NumberMathNode*>(&source);
-        if (!node) return nullptr;
-
-        auto base = utils::makeWidgetWithLayout();
-        auto* combo = new QComboBox();
-        base->layout()->addWidget(combo);
-        combo->addItems(QStringList{"+", "-", "*", "/", "pow"});
-
-        auto const toText = [](NumberMathNode::MathOperation op) {
-            switch (op)
-            {
-            case NumberMathNode::Minus:
-                return QStringLiteral("-");
-            case NumberMathNode::Divide:
-                return QStringLiteral("/");
-            case NumberMathNode::Multiply:
-                return QStringLiteral("*");
-            case NumberMathNode::Power:
-                return QStringLiteral("pow");
-            case NumberMathNode::Plus:
-                break;
-            }
-            return QStringLiteral("+");
-        };
-
-        auto const fromText = [](QString const& text) {
-            if (text == QStringLiteral("-")) return NumberMathNode::Minus;
-            if (text == QStringLiteral("*")) return NumberMathNode::Multiply;
-            if (text == QStringLiteral("/")) return NumberMathNode::Divide;
-            if (text == QStringLiteral("pow")) return NumberMathNode::Power;
-            return NumberMathNode::Plus;
-        };
-
-        auto const update = [node, combo, toText]() {
-            combo->setCurrentText(toText(node->operation()));
-        };
-
-        QObject::connect(node, &NumberMathNode::operationChanged,
-                         combo, update);
-
-        QObject::connect(combo, &QComboBox::currentTextChanged,
-                         combo, [node, fromText](QString const& text) {
-            auto next = fromText(text);
-            if (next == node->operation()) return;
-            node->setOperation(next);
-        });
-
-        update();
-
-        return convertToGraphicsWidget(std::move(base), object);
-    };
+    return &NumberMathNodeWidget::create;
 }
--- src/intelli/gui/ui/node/numbermathnodeui.h
+++ src/intelli/gui/ui/node/numbermathnodeui.h
@@ -9,9 +9,40 @@
 #define GT_INTELLI_NUMBERMATHNODEUI_H
 
 #include <intelli/gui/nodeui.h>
+#include <intelli/node/numbermath.h>
+#include <QWidget>
+
+class QComboBox;
 
 namespace intelli
 {
+
+class NumberMathNode;
+class NodeGraphicsObject;
+
+class NumberMathNodeWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit NumberMathNodeWidget(NumberMathNode& node, QWidget* parent = nullptr);
+
+    static NodeUI::QGraphicsWidgetPtr create(Node& source, NodeGraphicsObject& object);
+
+private slots:
+
+    void updateComboFromNode();
+    void updateNodeFromCombo(QString const& text);
+
+private:
+
+    static QString toText(NumberMathNode::MathOperation op);
+    static NumberMathNode::MathOperation fromText(QString const& text);
+
+    NumberMathNode* m_node{};
+    QComboBox* m_combo{};
+};
 
 class NumberMathNodeUI : public NodeUI
 {
